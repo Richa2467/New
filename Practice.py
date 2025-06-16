64bc4272306d1b7e6bb1356f35d95e5761410d4e
richatyagi <richa.tyagi@acldigital.com>
2025-06-16 16:31:44 +0530

    Second version of my code

--- Practice.py
+++ Practice.py
@@ -1,6 +1,16 @@
-class Class:
-    def __init__(self, val=0):
-        pass
+class A:
+    def __init__(self,v = 1):
+        self.v = v
+
+    def set(self,v):
+        self.v = v
+        return v
+
+
+a = A()
+print(a.set(a.v + 1))
+
+
 
 
 
